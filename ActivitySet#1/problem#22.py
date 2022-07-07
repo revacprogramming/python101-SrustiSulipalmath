d3b96b01b8f9578b0cf353eb2b3d38f17ed9e0cf
Srusti Sulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2022-07-07 16:05:37 +0000

    done

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,25 +1,17 @@
-def prepare_data(corruption_matrix, gold_fraction=0.5, merge_valset=True):
-    np.random.seed(1)
+def test_respect_dtype_singleton(self):
+        # See gh-7203
+        for dt in self.itype:
+            lbnd = 0 if dt is np.bool_ else np.iinfo(dt).min
+            ubnd = 2 if dt is np.bool_ else np.iinfo(dt).max + 1
 
-    mnist_images = np.copy(mnist.train.images)
-    mnist_labels = np.copy(mnist.train.labels)
-    if merge_valset:
-        mnist_images = np.concatenate([mnist_images, np.copy(mnist.validation.images)], axis=0)
-        mnist_labels = np.concatenate([mnist_labels, np.copy(mnist.validation.labels)])
+            sample = self.rfunc(lbnd, ubnd, dtype=dt)
+            assert_equal(sample.dtype, np.dtype(dt))
 
-    indices = np.arange(len(mnist_labels))
-    np.random.shuffle(indices)
+        for dt in (bool, int, np.long):
+            lbnd = 0 if dt is bool else np.iinfo(dt).min
+            ubnd = 2 if dt is bool else np.iinfo(dt).max + 1
 
-    mnist_images = mnist_images[indices]
-    mnist_labels = mnist_labels[indices].astype(np.long)
-
-    num_gold = int(len(mnist_labels)*gold_fraction)
-    num_silver = len(mnist_labels) - num_gold
-
-    for i in range(num_silver):
-        mnist_labels[i] = np.random.choice(num_classes, p=corruption_matrix[mnist_labels[i]])
-
-    dataset = {'x': mnist_images, 'y': mnist_labels}
-    gold = {'x': dataset['x'][num_silver:], 'y': dataset['y'][num_silver:]}
-
-    return dataset, gold, num_gold, num_silver 
+            # gh-7284: Ensure that we get Python data types
+            sample = self.rfunc(lbnd, ubnd, dtype=dt)
+            assert_(not hasattr(sample, 'dtype'))
+            assert_equal(type(sample), dt) 
